64febb962c85eac795c51021f78ce5f03c341ecd
UchihaaSasuke <cdrevision@gmail.com>
2022-12-10 16:55:50 +0100

    next.config.js updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   basePath: "/aizen-react-nextjs",
-//   reactStrictMode: true,
-// };
+// @ts-check
 
-// module.exports = nextConfig;
-
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
   basePath: "/aizen-react-nextjs",
-  assetPrefix: "/aizen-react-nextjs/",
 };
+
+module.exports = nextConfig;
+
+// module.exports = {
+//   basePath: "/aizen-react-nextjs",
+//   assetPrefix: "/aizen-react-nextjs/",
+// };
